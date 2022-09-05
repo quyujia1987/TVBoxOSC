7714fea89a0b0cd8da152988a1af073ed501c86e
quyujia1987 <860157898@qq.com>
2022-09-05 10:37:06 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/long/A/raw/branch/master/osc");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
